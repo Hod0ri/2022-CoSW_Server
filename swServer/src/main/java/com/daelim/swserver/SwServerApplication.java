e30bc205d4b2039c815e51ca559df9a6324ec529
Hod0ri <minthod0ri@gmail.com>
2022-10-26 11:20:37 +0900

    [ API ] Initialize Spring Boot Project

---
+++ swServer/src/main/java/com/daelim/swserver/SwServerApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.swserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwServerApplication.class, args);
+    }
+
+}
---
+++ swServer/src/test/java/com/daelim/swserver/SwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daelim.swserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
