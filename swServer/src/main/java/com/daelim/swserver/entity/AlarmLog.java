23f41e2f76d7648e7d2b2cf5b2920d12d267c603
qsef1256 <qsef1256@naver.com>
2022-10-28 23:44:20 +0900

    [ JPA ] add table name of AlarmLog

--- swServer/src/main/java/com/daelim/swserver/entity/AlarmLog.java
+++ swServer/src/main/java/com/daelim/swserver/entity/AlarmLog.java
@@ -2,9 +2,11 @@ package com.daelim.swserver.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "tb_alarmlog")
 public class AlarmLog {
 
     @Id
