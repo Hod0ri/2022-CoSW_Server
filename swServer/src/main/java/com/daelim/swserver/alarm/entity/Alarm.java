7f77e1fce60b346fff5b2bc656e04f1651a7b646
Hod0ri <minthod0ri@gmail.com>
2022-10-31 01:19:42 +0900

    [ API ] JPA Alarm

--- swServer/src/main/java/com/daelim/swserver/alarm/entity/Alarm.java
+++ swServer/src/main/java/com/daelim/swserver/alarm/entity/Alarm.java
@@ -1,14 +1,12 @@
 package com.daelim.swserver.alarm.entity;
 
+import com.daelim.swserver.auth.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,13 +16,26 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@SequenceGenerator(
+        name = "ALARM_SEQ_GENERATOR",
+        sequenceName = "ALARM_SEQ",
+        initialValue = 1,
+        allocationSize = 1
+)
 public class Alarm {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ALARM_SEQ_GENERATOR")
     private String alarmId;
-
+    @Column(nullable = false, columnDefinition = "TEXT")
     private LocalDateTime startTime;
+    @Column(nullable = false, columnDefinition = "TEXT")
     private int days;
+    @Column(nullable = false, columnDefinition = "TEXT")
     private boolean isRepeat;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
 
 }
